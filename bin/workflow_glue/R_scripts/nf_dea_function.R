062f8f4d8666936df1fa8ffd6e56b9815ca8a85f
Lioba Lehmann <115792969+LaLioba@users.noreply.github.com>
2024-03-06 16:08:34 +0100

    Update nf_dea_function.R
    Added if else function which configures the counttable depending on the type of analysis

--- bin/workflow_glue/R_scripts/nf_dea_function.R
+++ bin/workflow_glue/R_scripts/nf_dea_function.R
@@ -1,16 +1,40 @@
 
 args = commandArgs(trailingOnly=TRUE)
 input_metadata <- args[1]
-input_counttable <- args[2]
-input_threads <- args[3]
-output_file <- args[4]
+input_gene_or_trans <- args[2]
+input_counttable <- args[3]
+input_threads <- args[4]
+output_file <- args[5]
 
 # read metadata file (fread)
 # input_metadata = path/to/metadata_file, piped in by nextflow pipeline
 metadata <- fread(input_metadata)
 
-# fread count table: input_counttable = path/to/counttable, piped in by nextflow pipeline
-counts <- fread(input_counttable)
+
+if(input_gene_or_trans == "gene"){
+  # fread count table: input_counttable = path/to/counttable, piped in by nextflow pipeline
+  counttable <- fread(input_counttable, data.table = F)
+  geneNames = counts$Geneid
+  counttable$Geneid <- NULL
+  counts_numeric = sapply(counttable, as.numeric)
+  row.names(counts_numeric) = geneNames
+  nulls = sapply(0,function(counts)rowSums(counts_numeric==counttable))
+  num_nulls = length(which(nulls[,1] == 0))
+  
+  # make counts global variable
+  counts$df <<- as.data.frame(counts_numeric)
+  
+  } else if (input_gene_or_trans == "transcript"){
+    counttable = fread(input_counttable, data.table = T)
+    geneNames = counttable$Name
+    counttable = counttable %>% dplyr::select(-1:-4)
+    counttable = as.data.frame(counttable)
+    counts_numeric = sapply(counttable, as.numeric)
+    row.names(counts_numeric) = geneNames
+    nulls = sapply(0,function(counttable)rowSums(counts_numeric==counttable))
+    
+    countsfile$df_trans <<- as.data.frame(counts_numeric)
+}
 
 # threads as input parameter!
 register(MulticoreParam(input_threads))
@@ -58,4 +82,3 @@ run_preprocessing_dea <- function(metadata, counts, output_file){
 }
 
 run_preprocessing_dea(input_metadata, input_counttable, output_file)
-
